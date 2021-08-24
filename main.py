067783e67b0eb6dc1e40424ef5cfee2e0ea4c625
JT Raber <JMANN240raber@gmail.com>
2021-08-24 00:04:50 -0400

    settings support

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
 from os import urandom
+import json
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = urandom(24)
 socketio = SocketIO(app)
+
+with open("settings.json", "r") as settings_file:
+    settings = json.load(settings_file)
 
 @app.route('/')
 def index():
@@ -16,4 +20,8 @@ def on_candidate(cnd):
     emit('candidate', cnd)
 
 if (__name__ == '__main__'):
-    socketio.run(app, host='0.0.0.0', port=8000, debug=True)
+    socketio.run(app, host=settings.get("flask_host", "0.0.0.0"), 
+                 port=settings.get("port", 8000), 
+                 debug=settings.get("debug", False), 
+                 keyfile=settings.get("keyfile"), 
+                 certfile=settings.get("certfile"))
